b9e0170ea1fd12ee80a6ac6a036315c1b04956bd
Alkanar <alkanargj@gmail.com>
2016-03-09 15:02:40 +0100

    Some changes... cant remember what. Shit still doesnt work tho

--- Blad_ansvarlig_app/MainView.js
+++ Blad_ansvarlig_app/MainView.js
@@ -29,3 +29,11 @@ var data = Observable(progress);
 module.exports = {
     data: data
 };
+
+// Code for getting RSS
+// var Observable = require("FuseJS/Observable");
+// var data = Observable();
+// fetch('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.dagbladet.no/rss/nyheter/').then(function(response) { return response.json(); }).then(function(responseObject) { data.value = responseObject; });
+// module.exports = {
+//     dataSource: data
+// };
