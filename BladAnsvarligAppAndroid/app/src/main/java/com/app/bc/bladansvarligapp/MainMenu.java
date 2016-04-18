ebf4c1de5517d4c9ad6738f6e16beabdf6ad9423
Alkanar <alkanargj@gmail.com>
2016-04-18 14:30:17 +0200

    pushing to get working files on to another comp

---
+++ BladAnsvarligAppAndroid/app/src/main/java/com/app/bc/bladansvarligapp/MainMenu.java
@@ -0,0 +1,29 @@
+package com.app.bc.bladansvarligapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by alkan on 18.04.2016.
+ */
+public class MainMenu extends Activity {
+
+    private TextView mWelcome;
+    private TextView mBokerSolgt;
+    private TextView mPoeng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        mWelcome = (TextView) findViewById(R.id.welcome);
+        mBokerSolgt = (TextView) findViewById(R.id.bokerSolgt);
+        mPoeng = (TextView) findViewById(R.id.poeng);
+        mWelcome.setText("God jobb, Meny Coloseum!");
+        mBokerSolgt.setText("16");
+        mPoeng.setText("1");
+
+    }
+}
