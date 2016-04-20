6a8465f3b7d6bf6a01397df5d66e32980532f153
Alkanar <alkanargj@gmail.com>
2016-04-20 11:41:36 +0200

    Added layouts

---
+++ BladAnsvarligAppAndroid/app/src/main/java/com/app/bc/bladansvarligapp/Checklist.java
@@ -0,0 +1,17 @@
+package com.app.bc.bladansvarligapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by alkan on 20.04.2016.
+ */
+public class Checklist extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist);
+    }
+}
